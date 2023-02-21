253640e53ebfb8a0db16ecf7f43e8cdd90ee9f5e
kayceewong <kayceewonghk@gmail.com>
2023-02-21 14:16:01 +0800

    Updated passport.js

--- src/_middlewares/passport.js
+++ src/_middlewares/passport.js
@@ -1,52 +1,24 @@
-import AWS from 'aws-sdk'
-import fs from 'fs'
+// src/_middlewares/passport.js
+import passport from 'passport'
+import LocalStrategy from 'passport-local'
+import bcrypt from 'bcrypt'
 import _ from 'lodash'
 
-const s3 = new AWS.S3({
-  accessKeyId: process.env.S3_ACCESS_KEY,
-  secretAccessKey: process.env.S3_SECRET_KEY,
-  region: process.env.S3_REGION
-})
+import prisma from '../controllers/_helpers/prisma.js'
 
-const uploadFileAsync = async (data, { files }) => {
-  const fileKeys = Object.keys(files)
-
-  const promises = []
-
-  for (let i = 0; i < fileKeys.length; i += 1) {
-    const key = fileKeys[i]
-    const file = files[key]
-
-    if (_.get(data, key)) {
-      // Read content from the file
-      const fileContent = fs.readFileSync(file.filepath)
-
-      // Setting up S3 upload parameters
-      const params = {
-        ACL: 'public-read',
-        Bucket: process.env.S3_BUCKET,
-        Key: file.newFilename, // File name you want to save as in S3
-        Body: fileContent
-      }
-
-      // Replace the body file with a url
-      _.set(data, key, `https://${process.env.S3_BUCKET}.s3.${process.env.S3_REGION}.amazonaws.com/${file.newFilename}`)
-
-      // Uploading files to the bucket and push promise for Promise.all
-      promises.push(s3.upload(params).promise().then((uploadedFile) => {
-        fs.unlinkSync(file.filepath)
-        return uploadedFile.Location
-      }).catch((err) => {
-        console.log(err) // eslint-disable-line
-        fs.unlinkSync(file.filepath)
-        return err
-      }))
-    } else {
-      fs.unlinkSync(file.filepath)
-    }
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  session: false
+}, async (email, password, done) => {
+  try {
+    const user = await prisma.user.findFirst({ where: { email } })
+    if (!user) return done(null, false, { email: 'Email Not Found' })
+    if (!await bcrypt.compare(password, user.passwordHash)) return done(null, false, { password: 'Incorrect Password' })
+    return done(null, _.omit(user, ['passwordHash']))
+  } catch (err) {
+    return done(err)
   }
+}))
 
-  await Promise.all(promises)
-}
-
-export default uploadFileAsync
+export default passport
--- src/routes.js
+++ src/routes.js
@@ -3,3 +3,8 @@ import { Router } from 'express'
 const router = Router()
 
 export default router
+
+// src/routes.js
+
+// API | AUTH
+router.post('/api/auth/signup', (await import('./controllers/api/auth/signup.js')).default)
