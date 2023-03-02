673d0f07da3b7b86076edc65fab7c6f63c0ae049
kayceewong <kayceewonghk@gmail.com>
2023-03-02 11:18:18 +0800

    update

--- src/controllers/api/posts/show.js
+++ src/controllers/api/posts/show.js
@@ -6,7 +6,12 @@ import handleErrors from '../../_helpers/handle-errors.js'
 const controllersApiPostsShow = async (req, res) => {
   try {
     const { params: { id } } = req
-    const foundPost = await prisma.post.findUnique({ where: { id: Number(id) }, rejectOnNotFound: true })
+    const foundPost = await prisma.post.findUnique({
+      where: { id: Number(id) },
+      include: {
+        images: true
+      },
+      rejectOnNotFound: true })
     return res.status(200).json(foundPost)
   } catch (err) {
     return handleErrors(res, err)
