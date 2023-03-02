aed0e78dd005947728ca00f1fcdd68ba94e898fe
kayceewong <kayceewonghk@gmail.com>
2023-03-02 20:27:06 +0800

    update

--- src/controllers/api/my/posts/update.js
+++ src/controllers/api/my/posts/update.js
@@ -3,11 +3,17 @@ import yup from 'yup'
 import prisma from '../../../_helpers/prisma.js'
 import handleErrors from '../../../_helpers/handle-errors.js'
 import checkOwnership from './_check-ownership.js'
+import uploadFileAsync from '../../../_helpers/upload-file.js'
 
 const updateSchema = yup.object({
   content: yup.string().required(),
   images: yup.array().of(yup.object({
-    name: yup.string().required().label('image')
+    id: yup.mixed().notRequired(),
+    url: yup.mixed().when('id', {
+      is: (val) => !!val,
+      then: yup.mixed().notRequired(),
+      otherwise: yup.mixed().required()
+    }).label('Image')
   }))
 })
 
@@ -15,16 +21,14 @@ const controllersApiMyPostsUpdate = async (req, res) => {
   try {
     const { params: { id }, body } = req
     const verifiedData = await updateSchema.validate(body, { abortEarly: false, stripUnknown: true })
+    await uploadFileAsync(verifiedData, req)
     const updated = await prisma.post.update({
       where: { id: Number(id) },
       data: {
-        verifiedData,
+        ...verifiedData,
         images: {
-          set: [],
-          connectOrCreate: verifiedData?.images?.map((image) => ({
-            where: image,
-            create: image
-          }))
+          set: verifiedData?.images?.filter((img) => !!img.id)?.map((img) => ({ id: Number(img.id) })) || [],
+          create: verifiedData?.images?.filter((img) => !img.id) || []
         }
       }
     })
